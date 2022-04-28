070e4455b0236885e18473b9461aaad96adef88e
David Gonzalez Leon <david.gonzalez@citycable.ch>
2022-04-28 16:27:51 +0200

    updated fedora duplicate script

--- fedora_scripts/find_duplicates.sh
+++ fedora_scripts/find_duplicates.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 # finds duplicates created in OneDrive folder
+
+target="*hp-david*"
 echo "Searching duplicates..."
 cd /home/david/OneDrive/
-find . -name "*fedora*"
-lines=$(find . -name "*fedora*" | wc -l)
+find . -name $target
+lines=$(find . -name $target | wc -l)
 if [[ $lines -ne 0 ]]; then
    echo 'Do you want the duplicates files to be removed (y/n)?'
    read var
 
    if [[ "$var" = "y" ]]; then
-      find . -name "*fedora*" -exec rm -rf {} \;
+      find . -name $target -exec rm -rf {} \;
    fi
 else
    echo "No duplicates found!"
