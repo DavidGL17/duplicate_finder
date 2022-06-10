73dab74ee41c1e660678fc625cf42da0f86cd414
David Gonzalez Leon <david.gonzalez@citycable.ch>
2022-06-10 10:43:44 +0200

    small update to fedora script for updates

--- fedora_scripts/run_update.sh
+++ fedora_scripts/run_update.sh
@@ -2,5 +2,7 @@
 # This script allows to check for updates on all apps installed, including gnome extension
 echo 'Checking updates in dnf...'
 sudo dnf update
+echo 'Checking for obsolete packages...'
+sudo dnf autoremove
 echo 'Checking updates in gnome extensions...'
 gnome-shell-extension-installer --yes --update
