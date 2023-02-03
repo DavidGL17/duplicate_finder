ae2971f6e5ddc85e2c361f066d479dfef265e3a1
DavidGL17 <david.gonzalez@citycable.ch>
2023-02-03 11:01:57 +0100

    updated jpg to pdf script, all done+

--- jpg_to_pdf_converter/README.md
+++ jpg_to_pdf_converter/README.md
@@ -1,3 +1,27 @@
 # Jpg to pdf converter
 
 A python script for converting all .jpg files in a directory to .pdf. This script will take all ".jpg" files in the directory where this script is called, fuse them and save them as a single pdf file. It assumes that the files are named in the following way : "name-x.jpg" where x is a number. The name part cannot have a - in it.
+
+I mainly use it to create a pdf out of a Goodnotes file by exporting all pages as image, and then merging them into a single pdf. It is useful if your file is long and has a lot of pasted images in it, since sometimes the pdf conversion will bug out.
+
+## Usage
+
+1. Create a folder with the name of the pdf you want to create in a folder containing the script. For example, if you want to create a pdf called "my_pdf", create a folder called "my_pdf".
+2. Inside of this put all of your images. Each image should be named in the following way : "name-x.jpg" where x is a number. The name part cannot have a - in it.
+3. Execute the script. It will create a pdf file called "my_pdf.pdf" in the same folder as the script.
+
+The script will search all subdirectories for images, so you can have a folder structure like this :
+
+```
+my_pdf1
+|- image1-1.jpg
+|- image1-2.jpg
+|- ...
+my_pdf2
+|- image2-1.jpg
+|- image2-2.jpg
+|- ...
+script.py
+```
+
+And it will create two pdf files : "my_pdf1.pdf" and "my_pdf2.pdf".
--- jpg_to_pdf_converter/jpg_to_pdf_converter.py
+++ jpg_to_pdf_converter/jpg_to_pdf_converter.py
@@ -1,21 +1,35 @@
 import os
 from PIL import Image
 
-def jpg_to_pdf(path):
+
+def jpg_to_pdf(path, name):
+    # get all of the subdirectories in the specified directory
+
     # Get all of the .jpg files in the specified directory
-    jpg_files = [f for f in os.listdir(path) if f.endswith('.jpg')]
-  
+    jpg_files = [f for f in os.listdir(path) if f.endswith(".jpg")]
+
     # Sort the files by page number
-    jpg_files.sort(key=lambda x: int(x.split('-')[-1].split('.')[0]))
-   
+    jpg_files.sort(key=lambda x: int(x.split("-")[-1].split(".")[0]))
+
     # Create a list to hold the image objects
     images = []
-   
+
     # Open each image and add it to the list
     for jpg_file in jpg_files:
         images.append(Image.open(os.path.join(path, jpg_file)))
-   
-    # Save the images as a PDF
-    images[0].save(os.path.join(path, 'output.pdf'), save_all=True, append_images=images[1:])
 
-jpg_to_pdf('./')
+    # Save the images as a PDF
+    images[0].save(name, save_all=True, append_images=images[1:])
+
+
+if __name__ == "__main__":
+    # get all directories in the current directory
+    print("Getting all directories in the current directory")
+    directories = [d for d in os.listdir(".") if os.path.isdir(d)]
+    print("Found the following directories: {}".format(directories))
+    print("Converting each directory to a PDF")
+    for directory in directories:
+        print("Converting {} to PDF".format(directory))
+        jpg_to_pdf(directory, directory + ".pdf")
+        print("Done")
+    print("All done")
